2ece9a23c205cd61bf5f07298eb4312464e07d22
Raisess <raisechif@gmail.com>
2021-04-09 15:51:18 -0300

    setting up things

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory, NestApplication } from "@nestjs/core";
+
+import AppModule from "./app.module";
+
+const port: number | string = process.env.PORT || 1939;
+
+class App {
+	public static async bootstrap(): Promise<void> {
+		const app: NestApplication = await NestFactory.create(AppModule);
+
+		await app.listen(port, (): void => console.log("running on port:", port));
+	}
+}
+
+App.bootstrap();
+
