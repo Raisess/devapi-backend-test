406d485ba947fa83cd98d8dfe9789bc04d06a25d
Raisess <raisechif@gmail.com>
2021-04-10 14:45:27 -0300

    added require prop to connectors schema

--- src/connectors/schemas/connectors.schema.ts
+++ src/connectors/schemas/connectors.schema.ts
@@ -4,16 +4,16 @@ import { v4 as uuid } from "uuid";
 const { Schema } = mongoose;
 
 export const connectorsSchema: any = new Schema({
-  id:          { type: String, default: uuid() },
-  createdAt:   { type: Date, default: new Date().toLocaleString() },
-  updatedAt:   { type: Date, default: new Date().toLocaleString() },
-  name:        String,
-  type:        String,
-  privacy:     String,
-  baseUrl:     String,
-  logoUrl:     String,
-  category:    String,
-  description: String,
-  status:      String
+  id:          { type: String, default: uuid(), required: true },
+  createdAt:   { type: Date, default: new Date().toLocaleString(), required: true },
+  updatedAt:   { type: Date, default: new Date().toLocaleString(), required: true },
+  name:        { type: String, required: true },
+  type:        { type: String, required: true },
+  privacy:     { type: String, required: true },
+  baseUrl:     { type: String, required: true },
+  logoUrl:     { type: String, required: true },
+  category:    { type: String, required: true },
+  description: { type: String, required: true },
+  status:      { type: String, required: true }
 });
 
