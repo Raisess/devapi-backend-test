7adadb7746ed8168f9897f11ef78822c81bb44e6
Raisess <raisechif@gmail.com>
2021-04-10 16:15:04 -0300

    added nestjs swagger open api docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import "./database/connect";
 
 import { NestFactory, NestApplication } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
@@ -10,6 +11,12 @@ const port: number | string = process.env.PORT || 1939;
 class App {
   public static async bootstrap(): Promise<void> {
     const app: NestApplication = await NestFactory.create(AppModule);
+
+    const config: any = new DocumentBuilder().setTitle("DEVAPI: Connectors API").setDescription("API feita para teste de vaga de desenvolvedor backend Typescript/NestJS na devapi.").setVersion("1.0").build();
+
+    const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup("api", app, document);
 
     await app.listen(port, (): void => console.log("running on port:", port));
   }
