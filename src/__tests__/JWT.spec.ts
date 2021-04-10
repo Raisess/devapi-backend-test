401e14ae53f8a3549a9efc32eda0d1e8c3f72457
Raisess <raisechif@gmail.com>
2021-04-10 10:42:31 -0300

    created jwt shared module and jwt spec tests

---
+++ src/__tests__/JWT.spec.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import JWT from "../shared/JWT";
+
+describe("JWT test suite", (): void => {
+	const jwt: JWT = new JWT();
+
+	let token: string;
+
+	test("create jwt", (): void => {
+		token = jwt.sign({ data: "meow" });
+
+		expect(typeof token).toBe("string");
+	});
+
+	test("check jwt", (): void => {
+		expect(jwt.verify(token)).toBe(true);
+	});
+});
+
