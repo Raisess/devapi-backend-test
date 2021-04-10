6501692924900514b9fe7ec839081c3048382c17
Raisess <raisechif@gmail.com>
2021-04-10 14:44:01 -0300

    updated exception thrower

--- src/common/middlewares/auth.middleware.ts
+++ src/common/middlewares/auth.middleware.ts
@@ -1,11 +1,11 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, HttpStatus, HttpException } from '@nestjs/common';
 import { Request, Response, NextFunction } from "express";
 
 import JWT from "../../shared/JWT";
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  public use(req: Request, res: Response, next: NextFunction): void | Response<any> {
+  public use(req: Request, res: Response, next: NextFunction): void {
     if (req.headers.authorization && req.headers.authorization.split(" ").includes("Bearer")) {
       const token: string = req.headers.authorization.split(" ")[1];
 
@@ -14,9 +14,7 @@ export class AuthMiddleware implements NestMiddleware {
       }
     }
 
-    return res.status(401).json({
-      log: "Invalid authorization token"
-    });
+    throw new HttpException("Invalid authorization token", HttpStatus.UNAUTHORIZED);
   }
 }
 
