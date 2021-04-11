9b884c3914180abd4ae048ce534de386d224c28b
Raisess <raisechif@gmail.com>
2021-04-11 10:07:31 -0300

    updated connectors schema

--- src/connectors/schemas/connectors.schema.ts
+++ src/connectors/schemas/connectors.schema.ts
@@ -4,16 +4,14 @@ import { v4 as uuid } from "uuid";
 const { Schema } = mongoose;
 
 export const connectorsSchema: any = new Schema({
-  id: { type: String, default: uuid(), required: true },
+  id: { type: String, default: uuid() },
   createdAt: {
     type: Date,
     default: new Date().toLocaleString(),
-    required: true,
   },
   updatedAt: {
     type: Date,
     default: new Date().toLocaleString(),
-    required: true,
   },
   name: { type: String, required: true },
   type: { type: String, required: true },
